7385733b19cc01ef2e02302cf53ecbb8dd5de185
bruh <cardboardboxpepe@gmail.com>
2021-07-23 00:29:29 +0000

    Moved data from listOfExercisers into the component itself Added a method to create <li>s with <a> tags in them to create a list of exercise and links Updated imports

--- src/mainPageComponent/mainpage.jsx
+++ src/mainPageComponent/mainpage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { ExerciseRouterPrefix } from "../constants";
+import AllExercises from "../data/listOfExercises";
 import "./mainpage.css";
+
+const createLinks = composites => {
+  return composites.map(composite => {
+    return (
+      <li>
+        <a href={ExerciseRouterPrefix + composite.path}>
+          {composite.props.title}
+        </a>
+      </li>
+    );
+  });
+};
 
 export function MainPage(props) {
   return (
     <div className="exerciseContainer">
-      <h1>{props.title}</h1>
+      <h1>"Welcome to Fitness McFitFace"</h1>
       <div className="exerciseInner">
         <h1>Main Page</h1>
-        <p>{props.innertext}</p>
+        <p>"This website is powered by react."</p>
+        <h2>
+          Here is a list of all the exercises catalogged within this website
+        </h2>
+        <ul>{createLinks(AllExercises)}</ul>
       </div>
     </div>
   );
